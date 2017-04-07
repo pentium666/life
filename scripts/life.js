4275fb63e855fc7ba87aabe6c5668bb77ae05018
Maximilian Carr <maximilianacarr@gmail.com>
2017-04-06 23:25:49 -0400

    got simulation and display working

--- scripts/life.js
+++ scripts/life.js
@@ -8,28 +8,57 @@ function elt(tag, cl) {
 	return el;
 }
 
+function toggle(x, y) {
+	var state = game.grid[y][x];
+	state = !state;
+	game.grid[y][x] = state;
+	display.set(y, x, state);
+}
+
+function grid(height, width) {
+	var arr = [];
+	for(var y = 0; y < height; y++) {
+		var row = [];
+		for(var x = 0; x < width; x++) {
+			row.push(0);
+		}
+		arr.push(row);
+	}
+	return arr;
+}
+
 function Display(game) {
 	this.game = game;
 	this.grid = document.createElement("table");
 	this.width = game.width;
 	this.height = game.height;
-	for(var i = 0; i < y; i++) {
+	for(var y = 0; y < this.height; y++) {
 		var row = document.createElement("tr");
-		for(var j = 0; j < x; j++) {
+		for(var x = 0; x < this.width; x++) {
 			var cell = elt("td", "cell off");
-			cell.addEventListener("mousedown", function() {
-				this.game.toggle(x, y);
-			});
+			cell.id = x + "-" + y;
+			cell.setAttribute("x", x);
+			cell.setAttribute("y", y);
+			cell.setAttribute("onclick", "toggle("+x+","+y+");");
 			row.appendChild(cell);
 		}
 		this.grid.appendChild(row);
 	}
+	Array.prototype.slice.call(this.grid);
 }
 
-Display.prototype.render = function (game) {
+Display.prototype.set = function(y, x, on) {
+	var cell = document.getElementById(x + "-" + y);
+	cell.classList.remove("on");
+	if(on) {
+		cell.classList.add("on");
+	}
+}
+
+Display.prototype.render = function(game) {
 	for(var y = 0; y < this.height; y++) {
 		for(var x = 0; x < this.width; x++) {
-			this.grid[x][y].className = game.grid[x][y] ? "on" : "off";
+			this.set(y, x, this.game.grid[y][x]);
 		}
 	}
 };
@@ -37,27 +66,59 @@ Display.prototype.render = function (game) {
 function Game(x, y) {
 	this.width = x;
 	this.height = y;
-	this.grid = [];
-	for(var i = 0; i < y; i++) {
-		var row = [];
-		for(var j = 0; j < x; j++) {
-			row[j] = false;
+	this.grid = grid(y, x);
+	console.log(this.grid);
+}
+
+//counts neighbors
+Game.prototype.neighbors = function(y, x) {
+	var n = 0;
+	for(var i = -1; i <= 1; i++) {
+		if(y+i >= 0 && y+i < this.height) {
+			for(var j = -1; j <= 1; j++) {
+				if(x+j >= 0 && x+j < this.width && this.grid[y+i][x+j]) {
+					n++;
+				}
+			}
 		}
-		grid.push(row);
 	}
+	n -= this.grid[y][x];
+	return n;
 }
 
-game.prototype.toggle = function(x, y) {
-	var state = this.grid[x][y];
-	state = !state;
-	this.grid.[x][y] = state;
+//moves to next step in simulation
+Game.prototype.advance = function () {
+	var next = grid(this.height, this.width);
 
-}
-
-game.prototype.advance = function () {
-	var next = [];
+	for(var y = 0; y < this.height; y++) {
+		for(var x = 0; x < this.width; x++) {
+			var cell = this.grid[y][x];
+			var neighbors = this.neighbors(y, x);
+			//action for live cells
+			if(cell) {
+				if(neighbors < 2 || neighbors > 3) {
+					cell = false;
+				}
+			}
+			else if(neighbors == 3) {
+				console.log(y, x);
+				cell = true;
+			}
+			next[y][x] = cell;
+		}
+	}
+	this.grid = next;
 };
 
-var game = new Game(10, 10);
+Game.prototype.next = function() {
+	game.advance();
+	display.render(this);
+};
+
+var game = new Game(30, 15);
 var display = new Display(game);
 document.getElementById("board").appendChild(display.grid);
+
+function start() {
+	setInterval(game.next, 500);
+}
